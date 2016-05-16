e20e64da952fcd53d4ee04a7f2ae4b50204aa7ce
Sarah DeBey <sarah.debey@gmail.com>
2016-05-16 13:05:47 -0700

    adds functionality so user can click on picture to reveal image and click again to hide image

--- js/game.js
+++ js/game.js
@@ -1,14 +1,7 @@
-//variable declaration
+//variable declarations
 var randomArray = randomNumber();
+var arrayOfCards = [];
 var filepathArray = ['file:///Users/sarahdebey/projecttwo/images/tree.jpg', 'file:///Users/sarahdebey/projecttwo/images/chicken.jpg', 'file:///Users/sarahdebey/projecttwo/images/cabin.png'];
-
-//getting div elements that hold the imgs
-// var firstPicture = document.getElementById('card1-div');
-// var secondPicture = document.getElementById('card2-div');
-// var thirdPicture = document.getElementById('card3-div');
-// var fourthPicture = document.getElementById('card4-div');
-// var fifthPicture = document.getElementById('card5-div');
-// var sixthPicture = document.getElementById('card6-div');
 
 //getting img elements
 var picOne = document.getElementById('card-one');
@@ -18,14 +11,14 @@ var picFour = document.getElementById('card-four');
 var picFive = document.getElementById('card-five');
 var picSix = document.getElementById('card-six');
 
-//generic picture constructor with name attribute
+//generic picture constructor with name, randomNumber, and filepath attributes
 function Picture(name, number, filepath) {
   this.name = name;
   this.randomNumber = number;
   this.filepath = filepath;
 };
 
-//instantiate 6 cards
+//instantiate 6 cards, with a name, random number and filepath, but doesn't know where it's going to sit yet....
 var tree = new Picture('tree', randomArray[0], filepathArray[0]);
 var tree2 = new Picture('tree', randomArray[1], filepathArray[0]);
 
@@ -48,53 +41,67 @@ function randomNumber() {
   return randoms;
 }
 
-//function to render 6 images to screen
-Picture.prototype.renderPictures = function(){
-  if (this.randomNumber == 1) {
-    picOne.src = this.filepath;
-    // picOne.style.display = 'none';
-  }
-  if (this.randomNumber == 2) {
-    picTwo.src = this.filepath;
-    // picTwo.style.visibility = 'hidden';
-  }
-  if (this.randomNumber == 3) {
-    picThree.src = this.filepath;
-    // picThree.style.visibility = 'hidden';
-  }
-  if (this.randomNumber == 4) {
-    picFour.src = this.filepath;
-    // picFour.style.visibility = 'hidden';
-  }
-  if (this.randomNumber == 5) {
-    picFive.src = this.filepath;
-    // picFive.style.visibility = 'hidden';
-  }
-  if (this.randomNumber == 6) {
-    picSix.src = this.filepath;
-    // picSix.style.visibility = 'hidden';
-  }
-};
+//function to create array of objects
+function createArrayOfCards() {
+  arrayOfCards.push(tree, tree2, chicken, chicken2, cabin, cabin2);
+  return arrayOfCards;
+}
 
-//call renderPictures function on each card
-tree.renderPictures();
-tree2.renderPictures();
-chicken.renderPictures();
-chicken2.renderPictures();
-cabin.renderPictures();
-cabin2.renderPictures();
-
-picOne.addEventListener('click', handleClicks);
-picTwo.addEventListener('click', handleClicks);
-picThree.addEventListener('click', handleClicks);
-picFour.addEventListener('click', handleClicks);
-picFive.addEventListener('click', handleClicks);
-picSix.addEventListener('click', handleClicks);
-
-//function to handle clicks
-function handleClicks(event) {
-  console.log(this.target.id);
-  if (this.target.id == 'picOne') {
-    picOne.style.visibility = 'visible';
+function toggleEventListeners(event) {
+  if (event.target.src == 'file:///Users/sarahdebey/projecttwo/images/green.jpg') {
+    showPictures(event);
+  } else if (event.target.src !== 'file:///Users/sarahdebey/projecttwo/images/green.jpg') {
+    showGreen(event);
   }
 }
+
+picOne.addEventListener('click', toggleEventListeners);
+picTwo.addEventListener('click', toggleEventListeners);
+picThree.addEventListener('click', toggleEventListeners);
+picFour.addEventListener('click', toggleEventListeners);
+picFive.addEventListener('click', toggleEventListeners);
+picSix.addEventListener('click', toggleEventListeners);
+
+function showGreen (event) {
+  event.target.src = 'file:///Users/sarahdebey/projecttwo/images/green.jpg';
+}
+
+function showPictures (event) {
+  for (var i = 0; i < arrayOfCards.length; i++) {
+    if (event.target.id == 'card-one') {
+      if (arrayOfCards[i].randomNumber == 1) {
+        picOne.src = arrayOfCards[i].filepath;
+      }
+    }
+    if (event.target.id == 'card-two') {
+      if (arrayOfCards[i].randomNumber == 2) {
+        picTwo.src = arrayOfCards[i].filepath;
+      }
+    }
+    if (event.target.id == 'card-three') {
+      if (arrayOfCards[i].randomNumber == 3) {
+        picThree.src = arrayOfCards[i].filepath;
+      }
+    }
+    if (event.target.id == 'card-four') {
+      if (arrayOfCards[i].randomNumber == 4) {
+        picFour.src = arrayOfCards[i].filepath;
+      }
+    }
+    if (event.target.id == 'card-five') {
+      if (arrayOfCards[i].randomNumber == 5) {
+        picFive.src = arrayOfCards[i].filepath;
+      }
+    }
+    if (event.target.id == 'card-six') {
+      if (arrayOfCards[i].randomNumber == 6) {
+        picSix.src = arrayOfCards[i].filepath;
+      }
+    }
+  }
+}
+
+createArrayOfCards();
+
+
+//Now have to create game logic. User clicks one picture and then another picture. If pictures are a match, remove all event listeners. If pictures are not a match, user should click to turn back over.
